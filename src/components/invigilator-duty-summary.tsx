6ade5fbf5ccb4b9250254796f1aa18e19feb9db7
Lokesh D <lokeshconnects@gmail.com>
2025-07-13 09:24:04 +0000

    I think it did not work, could you please leave equal amount of space be

--- src/components/invigilator-duty-summary.tsx
+++ src/components/invigilator-duty-summary.tsx
@@ -118,11 +118,11 @@ export function InvigilatorDutySummary({ invigilators, assignments }: Invigilato
               <Table>
                 <TableHeader>
                   <TableRow>
-                    <TableHead className="w-[70px] text-center">Sl.No</TableHead>
-                    <TableHead className="w-[120px]">Date</TableHead>
-                    <TableHead className="w-[80px]">Day</TableHead>
+                    <TableHead className="text-center">Sl.No</TableHead>
+                    <TableHead>Date</TableHead>
+                    <TableHead>Day</TableHead>
                     <TableHead>Subject</TableHead>
-                    <TableHead className="w-[180px]">Timings</TableHead>
+                    <TableHead>Timings</TableHead>
                   </TableRow>
                 </TableHeader>
                 <TableBody>

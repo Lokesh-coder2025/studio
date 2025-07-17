c43e4f17eb2aa2f9acb0516e48aab4ed6f623911
Lokesh D <lokeshconnects@gmail.com>
2025-07-17 17:04:42 +0000

    Change the color of the button Select a date calendar trigger in Examina

--- src/components/examinations-step.tsx
+++ src/components/examinations-step.tsx
@@ -381,6 +381,7 @@ export function ExaminationsStep({ collegeName, setCollegeName, examTitle, setEx
                   <PopoverTrigger asChild>
                     <FormControl>
                       <Button
+                        variant="outline"
                         className={cn("w-full justify-start text-left font-normal", !field.value && "text-muted-foreground")}
                       >
                         <CalendarIcon className="mr-2 h-4 w-4" />

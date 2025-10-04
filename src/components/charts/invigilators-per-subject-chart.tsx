1d6ac807bf5bf9b3cb1b2ff0e2963cfc7bee11c6
Lokesh D <lokeshconnects@gmail.com>
2025-10-04 09:35:46 +0000

    Under "Invigilator Allocation per Subject" position the subject titles i

--- src/components/charts/invigilators-per-subject-chart.tsx
+++ src/components/charts/invigilators-per-subject-chart.tsx
@@ -84,7 +84,7 @@ export function InvigilatorsPerSubjectChart({ data, onTitleClick, isZoomed }: Ch
                 ))}
             </Pie>
             <ChartLegend 
-                content={<ChartLegendContent nameKey="subject" className={cn("grid gap-x-8 gap-y-2", isZoomed ? 'grid-cols-2' : '')} />} 
+                content={<ChartLegendContent nameKey="subject" className={cn("grid gap-x-8 gap-y-2", isZoomed ? 'grid-cols-3' : 'grid-cols-2')} />} 
             />
         </PieChart>
         </ResponsiveContainer>
